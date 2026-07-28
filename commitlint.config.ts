ac4d72b53e50528483742b94424757be1814d03d
Erick Machado <erick.gsmachado@gmail.com>
2026-07-28 11:21:25 -0300

    chore(tooling): add scope `workflow`

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,10 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "scope-case": [2, "always", "lower-case"],
-    "scope-enum": [2, "always", ["dependencies", "tooling"]],
-    "scope-empty": [2, "never"],
-    "subject-case": [2, "always", "lower-case"],
-    "type-case": [2, "always", "lower-case"],
-  },
-};
+    'scope-case': [2, 'always', 'lower-case'],
+    'scope-enum': [2, 'always', ['dependencies', 'tooling', 'workflow']],
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'type-case': [2, 'always', 'lower-case']
+  }
+}
