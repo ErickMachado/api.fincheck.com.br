9fb9c91fe1a30b7282caf5a615e2a8dfd892f2c3
Erick Machado <erick.gsmachado@gmail.com>
2026-08-04 18:19:06 -0300

    chore(tooling): add scopes `common` and `main`

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,7 +2,11 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-case': [2, 'always', 'lower-case'],
-    'scope-enum': [2, 'always', ['ai', 'code', 'dependencies', 'env', 'tooling', 'workflow']],
+    'scope-enum': [
+      2,
+      'always',
+      ['ai', 'code', 'common', 'dependencies', 'env', 'main', 'tooling', 'workflow']
+    ],
     'scope-empty': [2, 'never'],
     'subject-case': [2, 'always', 'lower-case'],
     'type-case': [2, 'always', 'lower-case']
