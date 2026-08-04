21c20131f734224392be09cbaa34f37651b79415
Erick Machado <erick.gsmachado@gmail.com>
2026-08-04 11:39:07 -0300

    chore(tooling): add scope `env`

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,7 +2,7 @@ export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'scope-case': [2, 'always', 'lower-case'],
-    'scope-enum': [2, 'always', ['dependencies', 'tooling', 'workflow', 'ai', 'code']],
+    'scope-enum': [2, 'always', ['ai', 'code', 'dependencies', 'env', 'tooling', 'workflow']],
     'scope-empty': [2, 'never'],
     'subject-case': [2, 'always', 'lower-case'],
     'type-case': [2, 'always', 'lower-case']
