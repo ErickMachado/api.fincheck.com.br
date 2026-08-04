4e723805a4f7b99d1513c3b122da9d08d3164aa2
Erick Machado <erick.gsmachado@gmail.com>
2026-08-04 18:19:43 -0300

    feat(common): add class `Configuration`

---
+++ src/common/core/config.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+
+const CONFIG_SCHEMA = z.object({
+  APP_ENV: z.enum(['development', 'production', 'staging', 'test']),
+  APP_HOST: z.ipv4({ error: 'Must be a valid IPv4 address' }),
+  APP_PORT: z.coerce.number({ error: 'Must be a number greater or equal to 0' })
+})
+
+type ConfigValues = z.infer<typeof CONFIG_SCHEMA>
+
+export class Configuration {
+  private constructor(private readonly values: ConfigValues) {}
+
+  public get app() {
+    return {
+      env: this.values.APP_ENV,
+      host: this.values.APP_HOST,
+      port: this.values.APP_PORT
+    }
+  }
+
+  public static async from(value: NodeJS.ProcessEnv): Promise<Configuration> {
+    const values = await CONFIG_SCHEMA.parseAsync(value)
+
+    return new Configuration(values)
+  }
+}
