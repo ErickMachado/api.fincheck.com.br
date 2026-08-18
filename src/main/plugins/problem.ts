7c9a6e21884d8acc41e358f366b6aa105e46a7c6
Erick Machado <erick.gsmachado@gmail.com>
2026-08-18 19:48:56 -0300

    fix(common): problem being mapped to internal error

--- src/main/plugins/problem.ts
+++ src/main/plugins/problem.ts
@@ -3,10 +3,18 @@ import { ZodError } from 'zod'
 import { Problem } from '@common/http/problem'
 
 export function problem(error: Error, request: FastifyRequest, reply: FastifyReply) {
-  let response = Problem.internal()
+  let response: Problem = null
 
   if (error instanceof ZodError) {
     response = Problem.fromZod(error)
+  }
+
+  if (error instanceof Problem && !response) {
+    response = error
+  }
+
+  if (!response) {
+    response = Problem.internal()
   }
 
   response.setInstance(request.url)
